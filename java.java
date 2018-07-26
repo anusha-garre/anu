bc1de6a200c5f20b725f0fba254f3f6194f017ea
anusha-garre <41730901+anusha-garre@users.noreply.github.com>
2018-07-26 15:33:10 +0530

    Create java.java

---
+++ java.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner input = new Scanner(System.in);
+ 
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+ 
+        if(number == int) 
+            System.out.println("Number is natural and positive.");	// your code goes here
+	}
+}
